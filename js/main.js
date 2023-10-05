592a98731c17e96ee053909d71c0036f3818e36e
SandraIpiales <seipiales@uce.edu.ec>
2023-10-05 17:41:30 -0500

    modified:   carrusel.css 	modified:   estilos.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
         duration:150,
         dist:-90,
         shift:5,
-        padding:5,
+        padding:4,
         numVisible:5,
         indicators:true,
         noWrap:true,
 
-        
 
     });
 });
